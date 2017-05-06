de73b548b7d4dacad323e8e412ce630e7dc6594f
David Higgins <thehig@gmail.com>
2017-05-06 13:35:44 +0100

    Override the User blueprint find action

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,16 @@
  */
 
 module.exports = {
-	
+	find: function(req, res) {
+    return req._sails.models.user.find().then(function(user){
+      console.log('find', user);
+      return res.ok(user);
+    }, function(err){
+      return res.serverError(err);
+    });
+  },
+  // create: function(req, res) { console.log('create'); return; },
+  // update: function(req, res) { console.log('update'); return; },
+  // destroy: function(req, res) { console.log('destroy'); return; },
 };
 
