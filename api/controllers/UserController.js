e210be11e8cd6da9192f651324ab6d251fdc901d
David Higgins <thehig@gmail.com>
2017-05-06 18:45:55 +0100

    Insert override for User findOne

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,17 +5,29 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var actionUtil = require('../../node_modules/sails/lib/hooks/blueprints/actionUtil.js');
 
 module.exports = {
-  // find: function(req, res) {
-  //   return req._sails.models.user.find()
-  //   .then(
-  //     (result)=> res.ok(result)
-  //     , (err)=> res.serverError(err)
-  //   );
-  // },
-  // create: function(req, res) { console.log('create'); return; },
-  // update: function(req, res) { console.log('update'); return; },
-  // destroy: function(req, res) { console.log('destroy'); return; },
+  // Implementation code taken from ./node_modules/sails/lib/hooks/blueprints/actions/findOne.js
+  findOne: function(req, res) {
+    var Model = actionUtil.parseModel(req);
+    var pk = actionUtil.requirePk(req);
+
+    var query = Model.findOne(pk);
+    query = actionUtil.populateRequest(query, req);
+    query.exec(function found(err, matchingRecord) {
+      if (err) { return res.serverError(err); }
+      if (!matchingRecord) { return res.notFound('No record found with the specified `id`.'); }
+
+      // Insert ICNDB joke here
+
+      if (req._sails.hooks.pubsub && req.isSocket) {
+        Model.subscribe(req, matchingRecord);
+        actionUtil.subscribeDeep(req, matchingRecord);
+      }
+
+      res.ok(matchingRecord);
+    });
+  }
 };
 
